d7ed6b60c8b984b32b04fe6dd41091a14ab2516a
jangbigom <jangbigom1384@naver.com>
2021-04-22 16:37:33 +0900

    index, about, layout, _navbar 완성

--- app.py
+++ app.py
@@ -4,5 +4,14 @@ app = Flask(__name__)
 
 app.debug = True
 
+@app.route('/', methods=['GET'])
+def index():
+  # return "Hello World"
+  return render_template("index.html", data="KIM")
+
+@app.route('/about')
+def about():
+  return render_template("about.html", hello = "Gary Kim")
+
 if __name__ == '__main__':
   app.run()
